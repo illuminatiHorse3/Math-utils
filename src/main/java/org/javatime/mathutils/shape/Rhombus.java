f2a64beee8c3eb41a3bd9af69b68eeba519190b0
risfy <14286113+risbah@users.noreply.github.com>
2020-06-01 03:20:52 -0400

    Add area and permiter methods to Rhombus class

--- src/main/java/org/javatime/mathutils/shape/Rhombus.java
+++ src/main/java/org/javatime/mathutils/shape/Rhombus.java
@@ -1,4 +1,25 @@
 package org.javatime.mathutils.shape;
 
-public class Rhombus {
+public class Rhombus{
+
+    public static Integer area(Integer a, Integer b){
+        return (a * b)/2;
+    }
+    public static Long area(Long a, Long b){
+        return (a*b)/2;
+    }
+    public static Double area(Double a, Double b){
+        return (a*b)/2;
+    }
+
+
+    public static Integer perimeter(Integer a){
+        return 4*a;
+    }
+    public static Long perimeter(Long a){
+        return 4*a;
+    }
+    public static Double perimeter(Double a){
+        return 4*a;
+    }
 }
